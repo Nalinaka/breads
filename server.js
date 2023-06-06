476aa586a07981b24d2569944cf29afb64ba4d47
Nalinaka <nalinaka@gmail.com>
2023-06-05 19:26:33 -0700

    FIxed

--- server.js
+++ server.js
@@ -6,44 +6,31 @@ const methodOverride = require('method-override')
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
-const app = express()
+console.log(PORT)
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to the World of Bread!')
+})
 
 //middleware
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  app.use(express.urlencoded({extended: true}))
-  app.use(methodOverride('_method'))
-  app.set('views', __dirname + '/views')
-  app.set('view engine', 'jsx')
-  app.engine('jsx', require('express-react-views').createEngine())
-  app.use(express.static('public'))
+const breadsController = require('./controllers/breads_controller.js')
+app.use(methodOverride('_method'))
+app.use('/breads', breadsController)
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
- 
-
-
-// ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
     // 404 Page
-app.get('*', (req, res) => {
-    res.send('404')
-  })
-
-// CREATE
-breads.post('/', (req, res) => {
-  console.log(req.body)
-  if(req.body.hasGluten === 'on') {
-    req.body.hasGluten = 'true'
-  } else {
-    req.body.hasGluten = 'false'
-  }
-  Bread.push(req.body)
-  res.redirect('/breads')
-})
-
-  })
-  
-// LISTEN
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
-})
+    
+   
+    app.get('*', (req, res) => {
+        res.send('404')
+      })
+    
+    // LISTEN
+    app.listen(PORT, () => {
+      console.log('listening on port', PORT);
+    })
