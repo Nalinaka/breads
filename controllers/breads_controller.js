e0adbfa20f45cfea3c25e65305c45f683f0e8e1c
Nalinaka <nalinaka@gmail.com>
2023-06-05 20:55:45 -0700

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -7,6 +7,14 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 
 // SHOW
 breads.get('/', (req, res) => {
@@ -15,8 +23,11 @@ breads.get('/', (req, res) => {
       breads: Bread
   })
   })
+
+
   
 // INDEX - render means "show this"
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -43,6 +54,17 @@ breads.post('/', express.urlencoded({ extended: true }), (req, res) => {
     Bread.push(req.body)
     res.redirect('/breads')
   })
+
+  // UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
 
 // DELETE
 breads.delete('/:indexArray', (req, res) => {
--- views/show.jsx
+++ views/show.jsx
@@ -1,5 +1,5 @@
 const React = require('react')
-const Default = require('./layouts/default')
+const Default = require('./layouts/Default')
 
 function Show({bread, index}){
     console.log(bread.name)
@@ -20,6 +20,7 @@ function Show({bread, index}){
     <form action={`/breads/${index}?_method=DELETE`} method="POST">
       <input type='submit' value="DELETE"/>
     </form>
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
       </Default>
     )}
 
